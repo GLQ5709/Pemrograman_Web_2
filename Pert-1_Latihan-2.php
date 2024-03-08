adde3eb857376e459fc2fae602795732e90745c9
GLQ5709 <146647753+GLQ5709@users.noreply.github.com>
2024-03-08 19:34:39 +0700

    Pert-1_Latihan-2.php

--- Pert-1_Latihan-2.php
+++ Pert-1_Latihan-2.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pertemuan 1 - Soal 2</title>
+    <title>Pertemuan 1 - Latihan Soal 2</title>
     <style>
         h1 {
             color: #333;
