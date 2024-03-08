4350294a17993ce6c2995980c9ca1f0e23ae79a8
GLQ5709 <146647753+GLQ5709@users.noreply.github.com>
2024-03-08 19:32:33 +0700

    Pert-1_Latihan-1.php

--- Pert-1_Latihan-1.php
+++ Pert-1_Latihan-1.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pertemuan 1 - Soal 1</title>
+    <title>Pertemuan 1 - Latihan Soal 1</title>
     <style>
         .tengah{
             margin-left: 450px;
